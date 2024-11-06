c4c0bf331bfcf25745bac010bcd7898c47254482
Estiven Meneses <estivenmeneses@ipaysolutions.co>
2024-11-05 20:59:17 -0500

    feat: template home desktop

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      'bg-test-100': '#f0f0f0',
+    },
+  },
+  plugins: [],
+}
+
